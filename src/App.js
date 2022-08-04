69d800ee35899ab4ce612ce4196b146a975372be
HelloRave <98197118+HelloRave@users.noreply.github.com>
2022-08-04 10:41:08 +0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import AboutUsPage from "./AboutUsPage";
+import LandingPage from "./LandingPage";
+import ContactUsPage from "./ContactUsPage";
+
+export default function App() {
+  return (
+    <div>
+      <LandingPage />
+      <AboutUsPage />
+      <ContactUsPage />
+    </div>
+  );
+}
